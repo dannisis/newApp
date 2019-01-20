d65ed88ceb8e2b1d974397ce6908b6e67874c941
0o <0o@0os-MacBook-Pro.local>
2019-01-20 04:52:10 +0100

    end of seccion 7

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../_services/auth.service';
 import {AlertifyService} from '../_services/alertify.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -14,7 +15,7 @@ export class NavComponent implements OnInit {
 
 
 
-  constructor(private  authservice: AuthService, private alertify: AlertifyService ) { }
+  constructor(private  authservice: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -26,6 +27,8 @@ export class NavComponent implements OnInit {
 
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -37,6 +40,7 @@ export class NavComponent implements OnInit {
   logOut() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
 
